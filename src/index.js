65a211ded018802c638884f0afd0556cf2ba9313
Tim Kye <Timothy.Kye@nike.com>
2016-08-16 15:38:34 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,94 @@
+'use strict'
+
+var assign = require('object-assign')
+var objectKeys = function(o,k,r){r=[];for(k in o)r.hasOwnProperty.call(o,k)&&r.push(k);return r} // eslint-disable-line
+
+var startTime = Date.now()
+var originalLog = console.log
+console.log = log
+
+var tokenizer = /{{(.+?)}}/g
+var logFormat = 'traceId={{traceId}} {{date}} appname={{appname}} version={{version}}'
+var logKeys = {}
+
+module.exports = logModule
+
+function logModule (handler) {
+  return function (event, context, callback) {
+    // Create initial values from context
+    setMdcKeys(context)
+
+    var next = function (err, result) {
+      finalLog(event, context, err, result)
+      callback(err, result)
+    }
+
+    // Replace context methods with next() callback
+    var logContext = assign({}, context, {
+      succeed: next.bind(null),
+      fail: function (error) { next(error) },
+      done: next
+    })
+
+    handler(event, logContext, next)
+  }
+}
+
+logModule.format = logFormat
+logModule.log = log
+logModule.setKey = setKey
+
+function setKey (keyName, value) {
+  logKeys[keyName] = value
+}
+
+function setMdcKeys (context) {
+  setKey('traceId', context.awsRequestId)
+  setKey('date', function () { return formatRfc3339(new Date(Date.now())) })
+  setKey('appname', context.functionName)
+  setKey('version', context.functionVersion)
+}
+
+function buildAccessLogPrefix () {
+  return logFormat.replace(tokenizer, getToken)
+}
+
+function getToken (match, key) {
+  var token = logKeys[key] || '((TOKEN MISSING: ' + key + '))'
+  return typeof token === 'function' ? token() : token
+}
+
+function log () {
+  return originalLog.apply(null, [buildAccessLogPrefix(), ' | '].concat(Array.prototype.slice.call(arguments)))
+}
+
+function finalLog (event, context, err, result) {
+  var finalValues = {
+    'requestURL': event.path,
+    'requestMethod': event.method,
+    'elapsedTime': startTime - Date.now(),
+    'accessToken': event.headerParams && event.headerParams.Authorization,
+    'apiKey': null,
+    'restApiId': event.requestId,
+    'restResourceId': null
+  }
+  var logValues = []
+  objectKeys(finalValues).forEach(function (key) {
+    if (finalValues[key]) logValues.push(key + '=' + finalValues[key])
+  })
+  log.apply(null, logValues)
+}
+
+// Utilities
+//
+
+function pad (n) { return n < 10 ? '0' + n : n }
+
+function formatRfc3339 (d) {
+  return d.getUTCFullYear() + '-' +
+    pad(d.getUTCMonth() + 1) + '-' +
+    pad(d.getUTCDate()) + 'T' +
+    pad(d.getUTCHours()) + ':' +
+    pad(d.getUTCMinutes()) + ':' +
+    pad(d.getUTCSeconds()) + 'Z'
+}
---
+++ test/logger.spec.js
@@ -0,0 +1,59 @@
+'use strict'
+
+var test = require('blue-tape')
+
+// TAP uses logs for test results
+// Since logger hijacks the console.log, we need to restore it, or TAP breaks
+// We can hijack logger's log though, and capture it's calls
+var originalLog = console.log
+var logCalls = []
+logCalls.last = () => logCalls.length ? logCalls[logCalls.length - 1] : null
+console.log = function () { logCalls.push(Array.prototype.slice.call(arguments)) }
+var logModule = require('../src/index')
+var loggerLog = console.log
+console.log = originalLog
+
+const defaultEvent = {
+  path: '/some/route',
+  method: 'GET',
+  headerParams: { Authorization: 'Bearer 12' },
+  requestId: 'request-id'
+}
+const defaultContext = {
+  awsRequestId: 'asdfghjkl',
+  functionName: 'test-function',
+  functionVersion: 'test-version'
+}
+const noop = () => {}
+const makeLogger = (event, context, callback) => {
+  var l = logModule((e, c, cb) => { cb(null, 'done') })
+  l(Object.assign({}, defaultEvent, event), Object.assign({}, defaultContext, context), callback || noop)
+}
+
+test('logger should return a function', t => {
+  t.ok(typeof logModule === 'function', 'logger is function')
+  t.end()
+})
+
+test('logger replace console.log', t => {
+  t.notEqual(originalLog, loggerLog, 'console.log replaced')
+  t.end()
+})
+
+test('logger prepends trace values on logs', t => {
+  makeLogger()
+  loggerLog('test')
+  var lastCall = logCalls.last()
+  t.ok(lastCall[0].match(/traceId=asdfghjkl (\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z) appname=test-function version=test-version/))
+  t.end()
+})
+
+test('logger creates access log when callback is called', t => {
+  t.plan(1)
+  makeLogger(null, null, (err, result) => {
+    if (err) t.end(err)
+    var lastCall = logCalls.last()
+    var accessLog = lastCall.slice(2).join(' ')
+    t.ok(accessLog.match(/requestURL=\/some\/route requestMethod=GET elapsedTime=-?\d+ accessToken=Bearer 12 restApiId=request-id/))
+  })
+})
